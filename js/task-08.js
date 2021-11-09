1b181034e99976867657ead955d264573e046378
Alexandr Sorokin <sanek1678@gmail.com>
2021-11-09 17:42:55 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,9 +1,18 @@
 const inputForm = document.querySelector('.login-form');
+const emailContent = inputForm.querySelector('input[name="email"]').textContent;
+const passwordContent = inputForm.querySelector('input[name="password"]').textContent;
 
 inputForm.addEventListener('submit', checkForm)
 
 function checkForm(event) {
-    event.preventDeault();
-
-    if(inputForm)
+    event.preventDefault();
+    const formObject = {};
+    if ( emailContent === '' || passwordContent === '') {
+        alert('Some fields are unfilled, please fill them and proceed')
+    }
+    else {
+        formObject.email = `${emailContent}`;
+        formObject.password = `${passwordContent}`;
+        console.log(formObject);
+    }
 }
