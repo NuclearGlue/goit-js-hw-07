851f99489920db202f62ac432dbae1e980b04a09
Alexandr Sorokin <sanek1678@gmail.com>
2021-11-09 22:00:28 +0200

    Finished

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,12 @@ const inputField = document.querySelector('#validation-input');
 
 inputField.addEventListener('blur', fieldValidation)
 
-function fieldValidation (event){
+function fieldValidation(event) {
+    event.preventDefault();
     if (inputField.value.length == inputField.dataset.length) {
         inputField.classList.remove('invalid');
         inputField.classList.add('valid');
+        
     }
     else {
  inputField.classList.remove('valid');
--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,54 @@
 const inputForm = document.querySelector('.login-form');
-const emailContent = inputForm.querySelector('input[name="email"]').textContent;
-const passwordContent = inputForm.querySelector('input[name="password"]').textContent;
+
+
 
 inputForm.addEventListener('submit', checkForm)
 
 function checkForm(event) {
     event.preventDefault();
-    const formObject = {};
-    if ( emailContent === '' || passwordContent === '') {
-        alert('Some fields are unfilled, please fill them and proceed')
+
+    const  {
+        elements: { email, password }
+    } = event.currentTarget; 
+    
+
+    if (email.value === "" || password.value === "" || password.value.trim() === "") {
+        return alert("Somethin wrong, perhaps one of ht field empty!")
     }
-    else {
-        formObject.email = `${emailContent}`;
-        formObject.password = `${passwordContent}`;
-        console.log(formObject);
-    }
+    else{
+        const formList = { email: email.value, password: password.value };
+        console.log(formList);
 }
+    
+    event.currentTarget.reset();
+    
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // const inputs = event.currentTarget.elements;
+    // const mail = inputs.email.value;
+    // const password = inputs.password.value;
+    // const inputInfo = {};
+   
+    //     if (mail === ''|| password === '') {
+    //         return alert("You forgot to fill one of input field, please fill it!")
+    //     }
+    //     else {
+    //         inputInfo[mail.name] = `${mail.value}`;
+    //         inputInfo[password.name] = `${password.value }`;
+    //     }
+
