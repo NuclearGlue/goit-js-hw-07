1d1cc509738274a5d9f3b4351b41d4abf3f4f2b2
Alexandr Sorokin <sanek1678@gmail.com>
2021-11-14 20:39:19 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const imagesList = document.querySelector('.gallery');
+
+const addImages = images.map((picture) =>
+  `<li><img src='${picture.url}' alt='${picture.alt}'> </li>`);
+
+imagesList.insertAdjacentHTML("beforeend", addImages.join(''));
+
 console.log(galleryItems);
