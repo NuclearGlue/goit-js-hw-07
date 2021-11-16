43cc3f9fe98fc2e1d2f0f77107d35a9eb697182a
Alexandr Sorokin <sanek1678@gmail.com>
2021-11-16 17:37:24 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+ 
 const imagesList = document.querySelector('.gallery');
 
-const addImages = images.map((picture) =>
-  `<li><img src='${picture.url}' alt='${picture.alt}'> </li>`);
-
+const addImages = galleryItems.map((picture) =>
+  `<div class="gallery__item"><a
+    class="gallery__link"
+    href="${picture.original}"
+  ><img  class="gallery__image" src='${picture.preview}' alt='${picture.description}' data-source = '${picture.original}'> </a>
+  </div>`);
 imagesList.insertAdjacentHTML("beforeend", addImages.join(''));
 
-console.log(galleryItems);
+
+imagesList.addEventListener('click', bigPicture);
+
+function bigPicture(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const imageUrl = event.target.dataset.source;
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+  
+    lightbox.classList.add('is-open');
+    lightboxImage.src = imageUrl;
+    lightboxImage.alt = event.target.alt;
+     
+window.addEventListener('keydown', keyPress);
+}
+
+closeButton.addEventListener('click', modalClose);
+
+function modalClose(event) {
+    lightbox.classList.remove('is-open');
+
+    lightboxImage.src = '';
+    lightboxImage.alt = '';
+
+    window.removeEventListener('keydown', keyPress);
+}
+
+overlayBox.addEventListener('click', modalClose);
+
+
+
+
+
+
